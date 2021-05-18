0fc0c100f6f5291ef3e14a3127fd9730eecb7c0d
megaybte <megaybte@megaybte-275E4E-275E5E>
2021-05-18 13:47:57 -0500

    implemente los botones: Resta, División y Multiplicación.

---
+++ src/g4s21p8pbcm/Datos.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package g4s21p8pbcm;
+
+/**
+ *
+ * @author megaybte
+ */
+public class Datos {
+    private String nombre;
+    private int edad;
+    private String mail;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+    
+    
+}
---
+++ src/g4s21p8pbcm/G4S21P8PBCM.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package g4s21p8pbcm;
+
+/**
+ *
+ * @author megaybte
+ */
+public class G4S21P8PBCM {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new FrmPrincipal().setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
